67e68a941970ea45e426f83e358be2d392a8d2f0
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2024-10-18 08:45:01 -0600

    first update

---
+++ backend/src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.setGlobalPrefix('api/v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  await app.listen(3001, () =>
+    console.log(`
+      running on port 3001 http://localhost:3001`),
+  );
+}
+bootstrap();
